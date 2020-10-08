7893a1171ae9ac060ed89fe568c46838ea9112ae
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-10-08 16:49:53 -0400

    styled components added

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledP = styled.p`
+  margin-bottom: 2%;
+`;
+
+const StyledImg = styled.img`
+  display: inline-block;
+  width: 55%;
+  padding: 10% 0;
+`;
 
 export default function Content(props) {
   const { explanation, img } = props;
   return (
     <div>
-      <article className="content-article">
-        <p className="content-paragraph">{explanation.explanation}</p>
-        <img src={img.url} className="content-img" alt="" />
-      </article>
+      <StyledP>{explanation.explanation}</StyledP>
+      <StyledImg src={img.url}></StyledImg>
     </div>
+    // <div>
+    //   <article className="content-article">
+    //     <p className="content-paragraph">{explanation.explanation}</p>
+    //     <img src={img.url} className="content-img" alt="" />
+    //   </article>
+    // </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  font-size: 350%;
+  margin-bottom: 2%;
+`;
+
+const StyledP1 = styled.p`
+  font-style: italic;
+  margin-bottom: 2%;
+`;
+
+const StyledP2 = styled.p`
+  font-style: italic;
+  margin-bottom: 2%;
+`;
 
 export default function Title(props) {
   const { title, date, author } = props;
-
   return (
     <div>
-      <article>
-        <h1 className="title-heading">{title.title}</h1>
-        <p className="title-author">{author.copyright}</p>
-        <p className="title-date">{date.date}</p>
-      </article>
+      <StyledH1>{title.title}</StyledH1>
+      <StyledP1>{` By: ${author.copyright}`}</StyledP1>
+      <StyledP2>{date.date}</StyledP2>
     </div>
+
+    // <div>
+    //   <article>
+    //     <h1 className="title-heading">{title.title}</h1>
+    //     <p className="title-author">{` By: ${author.copyright}`}</p>
+    //     <p className="title-date">{date.date}</p>
+    //   </article>
+    // </div>
   );
 }
